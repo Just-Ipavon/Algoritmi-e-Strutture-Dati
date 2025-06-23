d9b8ebd58d1643a8206f8d0288db7c515a1fffd6
Pasquale Ruotolo <77306367+Just-Ipavon@users.noreply.github.com>
2025-06-23 23:19:38 +0200

    .

--- EXTRA. Esercizi/Prova di Laboratorio/Esercizi/ordinamento topologico/ordinamentoTopologico - Copia.cpp
+++ EXTRA. Esercizi/Prova di Laboratorio/Esercizi/ordinamento topologico/ordinamentoTopologico - Copia.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <queue>
 #include <stack>
 #include <unordered_map>
-#include <algorithm>
-#include <functional>
-//UGUALE AL DFS SOLO CHE UTILIZZIAMO LO STACK
+
 using namespace std;
 
 const int WHITE = 0;
@@ -14,130 +11,95 @@ const int GREY = 1;
 const int BLACK = 2;
 
 template<typename T>
-class Node{
-    public:
-    T key, val;
-    T discovery = 0;
-    T finish = 0;
-    Node<T>* parent;
+class Node {
+public:
+    T val;
     int color = WHITE;
+    int discovery = 0;
+    int finish = 0;
+    Node<T>* parent = nullptr;
 
-    Node(T val) : val(val){}
+    Node(T val) : val(val) {}
 };
 
 template<typename T>
-class Edge{
-    public:
-    T weight;
-    Node<T>* source;
-    Node<T>* destination;
+class Graph {
+private:
+    int time = 0;
+    unordered_map<T, Node<T>*> node_map;
+    unordered_map<T, vector<T>> adj;
 
-    Edge(T weight, Node<T>* source, Node<T>* destination) : weight(weight), source(source), destination(destination){}
-};
-
-template<typename T>
-class Graph{
-    public:
-    T time = 0;
-    vector<Node<T>*> nodes;
-    vector<Edge<T>*> edges;
-    unordered_map<T, vector<pair<T,T>>> ver;
-
-    void addNode(Node<T>* node){
-        nodes.push_back(node);
+public:
+    ~Graph() {
+        for (auto& [_, node] : node_map) delete node;
     }
 
-    void addEdge(T weight, Node<T>* source, Node<T>* destination){
-        Edge<T>* edge = new Edge<T>(weight, source, destination);
-        edges.push_back(edge);
-        //ORIENTATO
-        ver[source->val].push_back(make_pair(destination->val, weight));
-        ver[destination->val].push_back(make_pair(source->val, weight));
+    void add_edge(T from, T to) {
+        if (!node_map.count(from)) node_map[from] = new Node<T>(from);
+        if (!node_map.count(to)) node_map[to] = new Node<T>(to);
+        adj[from].push_back(to);
     }
 
-    //PER CONVENZIONE MI SONO DICHIARATO L'OFSTREAM QUI
-
-    void DFS(ofstream &out){
-        stack<Node<T>*> topostack;
-        for(auto u: nodes){
-            u->color = WHITE;
-            u->parent = nullptr;
+    void topological_sort(ofstream& out) {
+        stack<Node<T>*> st;
+        for (auto& [_, node] : node_map) {
+            node->color = WHITE;
+            node->parent = nullptr;
         }
-        for(auto u: nodes){
-            if(u->color == WHITE){
-                DFS_VISIT(u, topostack);
-            }
+        time = 0;
+        for (auto& [_, node] : node_map) {
+            if (node->color == WHITE) dfs_visit(node, st);
         }
 
-        while(!topostack.empty()){
-            Node<T>* node = topostack.top();
-            out<<"Nodo in ordine topologico: "<<node->val<<endl;
-            topostack.pop();
+        while (!st.empty()) {
+            out << "Nodo in ordine topologico: " << st.top()->val << "\n";
+            st.pop();
         }
     }
 
-    void DFS_VISIT(Node<T>* u, stack<Node<T>*> &topostack){
+private:
+    void dfs_visit(Node<T>* u, stack<Node<T>*>& st) {
         u->color = GREY;
         u->discovery = ++time;
-        for(auto v: ver[u->val]){
-            Node<T>* node = nullptr;
-            for(auto n: nodes){
-                if(n->val == v.first){
-                    node = n;
-                    break;
-                }
-            }
-            if(node == nullptr){
-                continue;
-            }
-            if(node->val == WHITE){
-                node->parent = u;
-                DFS_VISIT(node, topostack);
+
+        for (T v_val : adj[u->val]) {
+            Node<T>* v = node_map[v_val];
+            if (v->color == WHITE) {
+                v->parent = u;
+                dfs_visit(v, st);
             }
         }
+
         u->color = BLACK;
         u->finish = ++time;
-        topostack.push(u);
+        st.push(u);
     }
 };
+
 
 int main() {
     ifstream in("input.txt");
-    int numnodes, numedges;
-    in>>numnodes>>numedges;
-    unordered_map<int,Node<int>*> nodesmap;
-    Graph<int> g;
+    ofstream out("outputtop.txt");
 
-    for(int i = 0; i < numedges; i++){
-        int weight, sourceval, desteval;
-        in>>weight>>sourceval>>desteval;
-
-        Node<int>* sourcen = nullptr;
-        auto itsource = nodesmap.find(sourceval);
-        if(itsource == nodesmap.end()){
-            sourcen = new Node<int>(sourceval);
-            nodesmap[sourceval] = sourcen;
-            g.addNode(sourcen);
-        } else {
-            sourcen = itsource->second;
-        }
-
-        Node<int>* desten = nullptr;
-        auto itdeste = nodesmap.find(desteval);
-        if(itdeste == nodesmap.end()){
-            desten = new Node<int>(desteval);
-            nodesmap[desteval] = desten;
-            g.addNode(desten);
-        } else {
-            desten = itdeste->second;
-        }
-        g.addEdge(weight, sourcen, desten);
+    if (!in || !out) {
+        cerr << "Errore apertura file." << endl;
+        return 1;
     }
 
+    int n, m;
+    in >> n >> m;
+
+    Graph<int> g;
+    for (int i = 0; i < m; ++i) {
+        int weight, from, to;
+        in >> weight >> from >> to;
+        g.add_edge(from, to); // peso ignorato per topologia
+    }
+
+    g.topological_sort(out);
+
     in.close();
-    ofstream out("outputtop.txt");
-    g.DFS(out);
     out.close();
-    cout << "File creato" << endl;
+    cout << "File creato\n";
     return 0;
 }
