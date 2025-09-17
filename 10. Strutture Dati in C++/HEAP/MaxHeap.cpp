d9202fc55bf92cc33c4562d4c049b5331c47c6ac
Pasquale Ruotolo <pachiruo@gmail.com>
2025-09-17 17:21:57 +0200

    .

--- 10. Strutture Dati in C++/HEAP/MaxHeap.cpp
+++ 10. Strutture Dati in C++/HEAP/MaxHeap.cpp
@@ -4,73 +4,47 @@
 using namespace std;
 
 template<typename T>
-class Node {
-public:
-    T k;
-    Node(T k) : k(k) {}
-};
-
-template<typename T>
 class MaxHeap {
 private:
-    vector<Node<T>*> data;
-    int heapsize;
+    vector<T> data;
 
-    void max_heapify(int i) {
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-        int largest = i;
-        if (l < heapsize && data[l]->k > data[largest]->k) largest = l;
-        if (r < heapsize && data[r]->k > data[largest]->k) largest = r;
+    void max_heapify(int i, int size) {
+        int l = 2*i + 1, r = 2*i + 2, largest = i;
+        if (l < size && data[l] > data[largest]) largest = l;
+        if (r < size && data[r] > data[largest]) largest = r;
         if (largest != i) {
             swap(data[i], data[largest]);
-            max_heapify(largest);
+            max_heapify(largest, size);
         }
     }
 
     void build() {
-        heapsize = (int)data.size();
-        for (int i = heapsize / 2 - 1; i >= 0; --i)
-            max_heapify(i);
+        for (int i = data.size()/2 - 1; i >= 0; --i)
+            max_heapify(i, data.size());
     }
 
 public:
-    MaxHeap(vector<Node<T>*>& vec) : data(vec), heapsize(vec.size()) {
-        build();
-    }
+    MaxHeap(const vector<T>& vec) : data(vec) { build(); }
 
-    // INCREASE_KEY: aumenta la chiave dell'elemento in posizione i e riaggiusta il MaxHeap
     void increase_key(int i, T key) {
-        if (i < 0 || i >= heapsize) {
-            cerr << "Indice fuori range!" << endl;
-            return;
-        }
-        if (key < data[i]->k) {
-            cerr << "Nuova chiave più piccola della chiave corrente!" << endl;
-            return;
-        }
-        data[i]->k = key;
-        // Risaliamo nella heap finché la proprietà del MaxHeap non è rispettata
-        while (i > 0 && data[(i - 1) / 2]->k < data[i]->k) {
-            swap(data[i], data[(i - 1) / 2]);
-            i = (i - 1) / 2;
+        if (key < data[i]) return; // nuova chiave più piccola, ignoriamo
+        data[i] = key;
+        while (i > 0 && data[(i-1)/2] < data[i]) {
+            swap(data[i], data[(i-1)/2]);
+            i = (i-1)/2;
         }
     }
 
-    // HEAPSORT che ordina in ordine crescente (usiamo MaxHeap)
     void heapsort() {
-        build();
-        int original_size = heapsize;
-        for (int i = heapsize - 1; i >= 1; --i) {
+        int n = data.size();
+        for (int i = n-1; i >= 1; --i) {
             swap(data[0], data[i]);
-            heapsize--;
-            max_heapify(0);
+            max_heapify(0, i);
         }
-        heapsize = original_size;
     }
 
     void print_heap(ostream& out) const {
-        for (auto n : data) out << n->k << " ";
+        for (auto v : data) out << v << " ";
         out << endl;
     }
 };
@@ -78,39 +52,21 @@ public:
 int main() {
     ifstream in("input.txt");
     ofstream out("output_max.txt");
-    if (!in || !out) {
-        cerr << "Errore apertura file input/output\n";
-        return 1;
-    }
+    if (!in || !out) return 1;
 
-    int val;
-    vector<Node<int>*> nodes;
+    vector<int> vals;
+    int v;
+    while (in >> v) vals.push_back(v);
 
-    while (in >> val) nodes.push_back(new Node<int>(val));
-    in.close();
+    MaxHeap<int> H(vals);
 
-    MaxHeap<int> H(nodes);
+    out << "Max Heap iniziale:\n"; H.print_heap(out);
 
-    out << "Max Heap iniziale:\n";
-    H.print_heap(out);
-
-    // Supponiamo di chiamare increase_key sull'indice 5 con nuova chiave 220000
-    int increase_index = 5;
-    int new_key = 220000;
-
-    H.increase_key(increase_index, new_key);
-    out << "\nMax Heap dopo increase_key(" << increase_index << ", " << new_key << "):\n";
-    H.print_heap(out);
+    H.increase_key(3, 50); // esempio
+    out << "\nMax Heap dopo increase_key:\n"; H.print_heap(out);
 
     H.heapsort();
-    out << "\nMax Heap dopo heapsort (ordinato crescente):\n";
-    H.print_heap(out);
-
-    out.close();
-
-    // Pulizia memoria
-    for (auto n : nodes) delete n;
+    out << "\nHeap ordinato crescente:\n"; H.print_heap(out);
 
     cout << "Operazioni completate. Controlla output_max.txt\n";
-    return 0;
 }
--- 10. Strutture Dati in C++/HEAP/MinHeap.cpp
+++ 10. Strutture Dati in C++/HEAP/MinHeap.cpp
@@ -4,73 +4,47 @@
 using namespace std;
 
 template<typename T>
-class Node {
-public:
-    T k;
-    Node(T k) : k(k) {}
-};
-
-template<typename T>
 class MinHeap {
 private:
-    vector<Node<T>*> data;
-    int heapsize;
+    vector<T> data;
 
-    void min_heapify(int i) {
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-        int smallest = i;
-        if (l < heapsize && data[l]->k < data[smallest]->k) smallest = l;
-        if (r < heapsize && data[r]->k < data[smallest]->k) smallest = r;
+    void min_heapify(int i, int size) {
+        int l = 2*i + 1, r = 2*i + 2, smallest = i;
+        if (l < size && data[l] < data[smallest]) smallest = l;
+        if (r < size && data[r] < data[smallest]) smallest = r;
         if (smallest != i) {
             swap(data[i], data[smallest]);
-            min_heapify(smallest);
+            min_heapify(smallest, size);
         }
     }
 
     void build() {
-        heapsize = (int)data.size();
-        for (int i = heapsize / 2 - 1; i >= 0; --i)
-            min_heapify(i);
+        for (int i = data.size()/2 - 1; i >= 0; --i)
+            min_heapify(i, data.size());
     }
 
 public:
-    MinHeap(vector<Node<T>*>& vec) : data(vec), heapsize(vec.size()) {
-        build();
-    }
+    MinHeap(const vector<T>& vec) : data(vec) { build(); }
 
-    // DECREASE_KEY: diminuisce la chiave dell'elemento in posizione i e riaggiusta il MinHeap
     void decrease_key(int i, T key) {
-        if (i < 0 || i >= heapsize) {
-            cerr << "Indice fuori range!" << endl;
-            return;
-        }
-        if (key > data[i]->k) {
-            cerr << "Nuova chiave più grande della chiave corrente!" << endl;
-            return;
-        }
-        data[i]->k = key;
-        // Risaliamo nella heap finché la proprietà del MinHeap non è rispettata
-        while (i > 0 && data[(i - 1) / 2]->k > data[i]->k) {
-            swap(data[i], data[(i - 1) / 2]);
-            i = (i - 1) / 2;
+        if (key > data[i]) return; // nuova chiave più grande, ignoriamo
+        data[i] = key;
+        while (i > 0 && data[(i-1)/2] > data[i]) {
+            swap(data[i], data[(i-1)/2]);
+            i = (i-1)/2;
         }
     }
 
-    // HEAPSORT che ordina in ordine decrescente (usiamo MinHeap)
     void heapsort() {
-        build();
-        int original_size = heapsize;
-        for (int i = heapsize - 1; i >= 1; --i) {
+        int n = data.size();
+        for (int i = n-1; i >= 1; --i) {
             swap(data[0], data[i]);
-            heapsize--;
-            min_heapify(0);
+            min_heapify(0, i);
         }
-        heapsize = original_size;
     }
 
     void print_heap(ostream& out) const {
-        for (auto n : data) out << n->k << " ";
+        for (auto v : data) out << v << " ";
         out << endl;
     }
 };
@@ -78,42 +52,19 @@ public:
 int main() {
     ifstream in("input.txt");
     ofstream out("output_min.txt");
-    if (!in || !out) {
-        cerr << "Errore apertura file input/output\n";
-        return 1;
-    }
+    vector<int> vals;
+    int v;
+    while (in >> v) vals.push_back(v);
 
-    int val;
-    vector<Node<int>*> nodes;
+    MinHeap<int> H(vals);
 
-    while (in >> val) nodes.push_back(new Node<int>(val));
-    in.close();
+    out << "Min Heap iniziale:\n"; H.print_heap(out);
 
-    MinHeap<int> H(nodes);
+    H.decrease_key(3, 30); // esempio
+    out << "\nMin Heap dopo decrease_key:\n"; H.print_heap(out);
 
-    out << "Min Heap iniziale:\n";
-    H.print_heap(out);
-
-    // Supponiamo di chiamare decrease_key sull'indice 3 (per esempio)
-    // con nuova chiave 30 (decremento chiave)
-    // Se vuoi index 53, assicurati che ci siano almeno 54 elementi in input.txt
-    int decrease_index = 3;
-    int new_key = 30;
-
-    H.decrease_key(decrease_index, new_key);
-    out << "\nMin Heap dopo decrease_key(" << decrease_index << ", " << new_key << "):\n";
-    H.print_heap(out);
-
-    // Heapsort in ordine decrescente
     H.heapsort();
-    out << "\nMin Heap dopo heapsort (ordinato decrescente):\n";
-    H.print_heap(out);
-
-    out.close();
-
-    // Pulizia memoria
-    for (auto n : nodes) delete n;
+    out << "\nHeap ordinato decrescente:\n"; H.print_heap(out);
 
     cout << "Operazioni completate. Controlla output_min.txt\n";
-    return 0;
 }
