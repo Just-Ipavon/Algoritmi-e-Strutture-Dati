0645d17d08f7cfe9e08167c71177f5b6ed103e37
Pasquale Ruotolo <pachiruo@gmail.com>
2025-09-17 22:46:04 +0200

    .

--- 10. Strutture Dati in C++/HASH/Indirizzamento/HashIndirizzamento.cpp
+++ 10. Strutture Dati in C++/HASH/Indirizzamento/HashIndirizzamento.cpp
@@ -1,116 +1,65 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <list>
+#include <string>
 using namespace std;
 
+template<typename K, typename V>
+struct Oggetto {
+    K chiave;
+    V valore;
+    bool occupied;
 
-template<typename T,typename S>
-class Oggetto{
-public:
-    T chiave;
-    S valore;
-
-    Oggetto(T chiave,S valore) : chiave(chiave), valore(valore) {}
+    Oggetto() : occupied(false) {}
+    Oggetto(K k, V v) : chiave(k), valore(v), occupied(true) {}
 };
 
-template<typename T, typename S>
-class Hash{
+template<typename K, typename V>
+class Hash {
+    int m; // dimensione tabella
+    Oggetto<K,V>* tabella;
+
+    size_t hash(K key) { return key % m; }
+
 public:
-    vector<list<Oggetto<T,S>>> tabella;
-    int m;
+    Hash(int size) : m(size) { tabella = new Oggetto<K,V>[m]; }
 
-    Hash(int m) : m(m){
-        tabella.resize(m);
-    }
-
-    int hash(T chiave){
-        return chiave % m;
-    }
-
-    void insert(T chiave, S valore){
-        int idx = hash(chiave);
-        for(auto& i : tabella[idx]){
-            if(i.chiave == chiave){
-                i.valore = valore;
-                return;
-            }
+    void insert(K key, V value) {
+        size_t idx = hash(key);
+        size_t start = idx;
+        while(tabella[idx].occupied) {
+            idx = (idx + 1) % m;
+            if(idx == start) { cerr << "Tabella piena!\n"; return; }
         }
-        tabella[idx].push_back(Oggetto<T,S>(chiave,valore));
+        tabella[idx] = Oggetto<K,V>(key, value);
     }
 
-    bool contiene(T chiave){
-        int idx = hash(chiave);
-        for(auto& i : tabella[idx])
-            if(i.chiave == chiave) return true;
-        return false;
-    }
-
-
-    void stampa(ofstream& out){
-        for(int i = 0; i < m; i++)
-            for(auto& j : tabella[i])
-                out << j.chiave << " -> " << j.valore << "\n";
-    }
-
-    S* find(T chiave){
-        int idx = hash(chiave);
-        for(auto& oggetto : tabella[idx]){
-            if(oggetto.chiave == chiave){
-                return &(oggetto.valore);
-            }
+    void stampa(ofstream& out) {
+        for(int i = 0; i < m; i++) {
+            if(tabella[i].occupied)
+                out << tabella[i].chiave << " -> " << tabella[i].valore << "\n";
         }
-        return nullptr;
     }
 
+    void leggiFile(const string& file) {
+        ifstream in(file);
+        int k;
+        string v;
+        char c;
+        while(in >> c && c == '<' && in >> k >> c && c == ',') {
+            getline(in, v, '>');
+            insert(k, v);
+        }
+    }
 };
 
-void leggiFile(Hash<int,string>& H, string file){
-    ifstream in(file);
-    int k;
-    string v;
-    char c;
-    while(in >> c && c == '<' && in >> k >> c && c == ','){
-        getline(in, v, '>');
-        H.insert(k,v);
-    }
-}
-bool disjoint( Hash<int,string>& A,  Hash<int,string>& B){
-    for(int i = 0; i<A.m;i++)
-        for(auto& j : A.tabella[i])
-            if(B.contiene(j.chiave)) return false;
-    return true;
-}
-
-
-
-
-int main()
-{
-    int M = 999;
-    Hash<int,string> H1(M), H2(M);
-    leggiFile(H1,"input1.txt");
-    leggiFile(H2,"input2.txt");
+int main() {
+    Hash<int,string> H(10); // hash table dimensione 10
+    H.leggiFile("input.txt");
 
     ofstream out("output.txt");
-    out << (disjoint(H1,H2) ? "Disgiunte\n" : "Non disgiunte");
+    H.stampa(out);
+    out.close();
 
-    out << "\n Tabella 1\n"; H1.stampa(out);
-    out << "\n Tabella 2\n"; H2.stampa(out);
-
-     int chiaveDaCercare;
-     cout << "inserisci la chiave da cercare: ";
-     cin >> chiaveDaCercare;
-    string* valoreTrovato = H1.find(chiaveDaCercare);
-
-    if (valoreTrovato != nullptr) {
-        cout << "Trovato! La chiave " << chiaveDaCercare << " ha valore: " << *valoreTrovato << "\n"<<endl;
-    } else {
-        cout << "La chiave non e' stata trovata nella tabella.\n"<<endl;
-    }
-
-
-
-
+    cout << "Hash table salvata su output.txt\n";
     return 0;
 }
